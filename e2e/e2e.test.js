e3c9fc5a2c287028a40594c2d2dc4cf2eb8caff7
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-10-10 09:03:25 +0300

    Попробовал заменить $ на querySelector

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -22,30 +22,30 @@ describe('Card validation form', () => {
 
   test('Should be Mastercard', async () => {
     await page.goto(baseUrl);
-    const form = await page.$('[data-id=validate-card-form]');
-    const input = await form.$('[data-id=validate-card-input]');
+    const form = await page.querySelector('[data-id=validate-card-form]');
+    const input = await form.querySelector('[data-id=validate-card-input]');
     await input.type('5536913801114162');
-    const submit = await form.$('[data-id=validate-card-submit]');
+    const submit = await form.querySelector('[data-id=validate-card-submit]');
     submit.click();
     await page.waitFor('[data-id=mastercard].active');
   }),
   test('Should be Visa', async () => {
-    const form = await page.$('[data-id=validate-card-form]');
-    const input = await form.$('[data-id=validate-card-input]');
+    const form = await page.querySelector('[data-id=validate-card-form]');
+    const input = await form.querySelector('[data-id=validate-card-input]');
     await input.click({clickCount: 16});
     await input.press('Backspace'); 
     await input.type('4276314268549132');
-    const submit = await form.$('[data-id=validate-card-submit]');
+    const submit = await form.querySelector('[data-id=validate-card-submit]');
     submit.click();
     await page.waitFor('[data-id=visa].active');
   }),
   test('Should be Mir', async () => {
-    const form = await page.$('[data-id=validate-card-form]');
-    const input = await form.$('[data-id=validate-card-input]');
+    const form = await page.querySelector('[data-id=validate-card-form]');
+    const input = await form.querySelector('[data-id=validate-card-input]');
     await input.click({clickCount: 16});
     await input.press('Backspace'); 
     await input.type('2591463395745613');
-    const submit = await form.$('[data-id=validate-card-submit]');
+    const submit = await form.querySelector('[data-id=validate-card-submit]');
     submit.click();
     await page.waitFor('[data-id=mir].active');
   })
